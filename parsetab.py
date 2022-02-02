05ad2d25231c1a8f679e051b1f57c4515e837742
Jahel_Leon <jleonro@unal.edu.co>
2022-02-02 17:20:19 -0500

    added basic Matrix operations

--- parsetab.py
+++ parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'leftPLUSMINUSleftMULTIPLYDIVIDEABS ARCCOS ARCSIN ARCTAN CEIL COS DIVIDE E ELSE EQUALS EXP FLOAT FLOOR IF INT LBRACE LBRACKET LOG LPAREN MINUS MULTIPLY NAME PI PLUS POWER RBRACE RBRACKET ROUND RPAREN SIN SQRT TAN THEN WHILE\n    calc : expression\n         | var_assign\n         | empty\n    \n    var_assign : NAME EQUALS expression\n    \n    expression : SIN LPAREN expression RPAREN\n                | COS LPAREN expression RPAREN\n                | TAN LPAREN expression RPAREN\n                | SQRT LPAREN expression RPAREN\n                | ARCSIN LPAREN expression RPAREN\n                | ARCCOS LPAREN expression RPAREN\n                | ARCTAN LPAREN expression RPAREN\n                | ABS LPAREN expression RPAREN\n                | FLOOR LPAREN expression RPAREN\n                | ROUND LPAREN expression RPAREN\n                | CEIL LPAREN expression RPAREN\n\n    \n    expression : expression MULTIPLY expression\n               | expression DIVIDE expression\n               | expression PLUS expression\n               | expression MINUS expression\n    \n    expression : INT\n               | FLOAT\n    \n    expression : NAME\n    \n    empty :\n    '
+_lr_signature = 'leftPLUSMINUSleftMULTIPLYDIVIDEABS ARCCOS ARCSIN ARCTAN CEIL COMMA COS DIVIDE E ELSE EQUALS EXP FLOAT FLOOR IF INT LBRACE LBRACKET LOG LPAREN MINUS MULTIPLY NAME PI PLUS RBRACE RBRACKET ROUND RPAREN SIN SQRT TAN THEN WHILE\n    calc : expression\n         | var_assign\n         | empty\n    \n    expression : LBRACKET array_elements RBRACKET\n    \n    array_elements : expression COMMA array_elements\n                   | expression\n    \n    var_assign : NAME EQUALS expression\n    \n    expression : SIN LPAREN expression RPAREN\n                | COS LPAREN expression RPAREN\n                | TAN LPAREN expression RPAREN\n                | SQRT LPAREN expression RPAREN\n                | ARCSIN LPAREN expression RPAREN\n                | ARCCOS LPAREN expression RPAREN\n                | ARCTAN LPAREN expression RPAREN\n                | ABS LPAREN expression RPAREN\n                | FLOOR LPAREN expression RPAREN\n                | ROUND LPAREN expression RPAREN\n                | CEIL LPAREN expression RPAREN\n\n    \n    expression : expression MULTIPLY expression\n               | expression DIVIDE expression\n               | expression PLUS expression\n               | expression MINUS expression\n    \n    expression : INT\n               | FLOAT\n    \n    expression : NAME\n    \n    empty :\n    '
     
-_lr_action_items = {'SIN':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,]),'COS':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,]),'TAN':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,]),'SQRT':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,]),'ARCSIN':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,]),'ARCCOS':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,]),'ARCTAN':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,]),'ABS':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,]),'FLOOR':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,]),'ROUND':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,]),'CEIL':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,]),'INT':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,]),'FLOAT':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,]),'NAME':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[18,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,]),'$end':([0,1,2,3,4,16,17,18,35,36,37,38,39,51,52,53,54,55,56,57,58,59,60,61,62,],[-23,0,-1,-2,-3,-20,-21,-22,-16,-22,-17,-18,-19,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,]),'MULTIPLY':([2,16,17,18,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,],[19,-20,-21,-22,-16,-22,-17,19,19,19,19,19,19,19,19,19,19,19,19,19,19,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,]),'DIVIDE':([2,16,17,18,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,],[20,-20,-21,-22,-16,-22,-17,20,20,20,20,20,20,20,20,20,20,20,20,20,20,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,]),'PLUS':([2,16,17,18,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,],[21,-20,-21,-22,-16,-22,-17,-18,-19,21,21,21,21,21,21,21,21,21,21,21,21,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,]),'MINUS':([2,16,17,18,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,],[22,-20,-21,-22,-16,-22,-17,-18,-19,22,22,22,22,22,22,22,22,22,22,22,22,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,]),'LPAREN':([5,6,7,8,9,10,11,12,13,14,15,],[23,24,25,26,27,28,29,30,31,32,33,]),'RPAREN':([16,17,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,52,53,54,55,56,57,58,59,60,61,62,],[-20,-21,-16,-22,-17,-18,-19,52,53,54,55,56,57,58,59,60,61,62,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14,-15,]),'EQUALS':([18,],[34,]),}
+_lr_action_items = {'LBRACKET':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,]),'SIN':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,]),'COS':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,]),'TAN':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,]),'SQRT':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,]),'ARCSIN':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,]),'ARCCOS':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,]),'ARCTAN':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,]),'ABS':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,]),'FLOOR':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,]),'ROUND':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,]),'CEIL':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,]),'INT':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,]),'FLOAT':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,18,]),'NAME':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[19,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,26,]),'$end':([0,1,2,3,4,17,18,19,26,39,40,41,42,43,56,58,59,60,61,62,63,64,65,66,67,68,],[-26,0,-1,-2,-3,-23,-24,-25,-25,-19,-20,-21,-22,-4,-7,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,]),'MULTIPLY':([2,17,18,19,25,26,39,40,41,42,43,45,46,47,48,49,50,51,52,53,54,55,56,58,59,60,61,62,63,64,65,66,67,68,],[20,-23,-24,-25,20,-25,-19,-20,20,20,-4,20,20,20,20,20,20,20,20,20,20,20,20,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,]),'DIVIDE':([2,17,18,19,25,26,39,40,41,42,43,45,46,47,48,49,50,51,52,53,54,55,56,58,59,60,61,62,63,64,65,66,67,68,],[21,-23,-24,-25,21,-25,-19,-20,21,21,-4,21,21,21,21,21,21,21,21,21,21,21,21,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,]),'PLUS':([2,17,18,19,25,26,39,40,41,42,43,45,46,47,48,49,50,51,52,53,54,55,56,58,59,60,61,62,63,64,65,66,67,68,],[22,-23,-24,-25,22,-25,-19,-20,-21,-22,-4,22,22,22,22,22,22,22,22,22,22,22,22,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,]),'MINUS':([2,17,18,19,25,26,39,40,41,42,43,45,46,47,48,49,50,51,52,53,54,55,56,58,59,60,61,62,63,64,65,66,67,68,],[23,-23,-24,-25,23,-25,-19,-20,-21,-22,-4,23,23,23,23,23,23,23,23,23,23,23,23,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,]),'LPAREN':([6,7,8,9,10,11,12,13,14,15,16,],[27,28,29,30,31,32,33,34,35,36,37,]),'COMMA':([17,18,25,26,39,40,41,42,43,58,59,60,61,62,63,64,65,66,67,68,],[-23,-24,44,-25,-19,-20,-21,-22,-4,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,]),'RBRACKET':([17,18,24,25,26,39,40,41,42,43,57,58,59,60,61,62,63,64,65,66,67,68,],[-23,-24,43,-6,-25,-19,-20,-21,-22,-4,-5,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,]),'RPAREN':([17,18,26,39,40,41,42,43,45,46,47,48,49,50,51,52,53,54,55,58,59,60,61,62,63,64,65,66,67,68,],[-23,-24,-25,-19,-20,-21,-22,-4,58,59,60,61,62,63,64,65,66,67,68,-8,-9,-10,-11,-12,-13,-14,-15,-16,-17,-18,]),'EQUALS':([19,],[38,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'calc':([0,],[1,]),'expression':([0,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,],[2,35,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,]),'var_assign':([0,],[3,]),'empty':([0,],[4,]),}
+_lr_goto_items = {'calc':([0,],[1,]),'expression':([0,5,20,21,22,23,27,28,29,30,31,32,33,34,35,36,37,38,44,],[2,25,39,40,41,42,45,46,47,48,49,50,51,52,53,54,55,56,25,]),'var_assign':([0,],[3,]),'empty':([0,],[4,]),'array_elements':([5,44,],[24,57,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -27,27 +27,30 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> calc","S'",1,None,None,None),
-  ('calc -> expression','calc',1,'p_calc','calc.py',117),
-  ('calc -> var_assign','calc',1,'p_calc','calc.py',118),
-  ('calc -> empty','calc',1,'p_calc','calc.py',119),
-  ('var_assign -> NAME EQUALS expression','var_assign',3,'p_var_assign','calc.py',125),
-  ('expression -> SIN LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',132),
-  ('expression -> COS LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',133),
-  ('expression -> TAN LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',134),
-  ('expression -> SQRT LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',135),
-  ('expression -> ARCSIN LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',136),
-  ('expression -> ARCCOS LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',137),
-  ('expression -> ARCTAN LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',138),
-  ('expression -> ABS LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',139),
-  ('expression -> FLOOR LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',140),
-  ('expression -> ROUND LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',141),
-  ('expression -> CEIL LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',142),
-  ('expression -> expression MULTIPLY expression','expression',3,'p_expression','calc.py',153),
-  ('expression -> expression DIVIDE expression','expression',3,'p_expression','calc.py',154),
-  ('expression -> expression PLUS expression','expression',3,'p_expression','calc.py',155),
-  ('expression -> expression MINUS expression','expression',3,'p_expression','calc.py',156),
-  ('expression -> INT','expression',1,'p_expression_int_float','calc.py',163),
-  ('expression -> FLOAT','expression',1,'p_expression_int_float','calc.py',164),
-  ('expression -> NAME','expression',1,'p_expression_var','calc.py',170),
-  ('empty -> <empty>','empty',0,'p_empty','calc.py',181),
+  ('calc -> expression','calc',1,'p_calc','calc.py',119),
+  ('calc -> var_assign','calc',1,'p_calc','calc.py',120),
+  ('calc -> empty','calc',1,'p_calc','calc.py',121),
+  ('expression -> LBRACKET array_elements RBRACKET','expression',3,'p_array','calc.py',127),
+  ('array_elements -> expression COMMA array_elements','array_elements',3,'p_array_array_elements','calc.py',133),
+  ('array_elements -> expression','array_elements',1,'p_array_array_elements','calc.py',134),
+  ('var_assign -> NAME EQUALS expression','var_assign',3,'p_var_assign','calc.py',144),
+  ('expression -> SIN LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',153),
+  ('expression -> COS LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',154),
+  ('expression -> TAN LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',155),
+  ('expression -> SQRT LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',156),
+  ('expression -> ARCSIN LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',157),
+  ('expression -> ARCCOS LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',158),
+  ('expression -> ARCTAN LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',159),
+  ('expression -> ABS LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',160),
+  ('expression -> FLOOR LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',161),
+  ('expression -> ROUND LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',162),
+  ('expression -> CEIL LPAREN expression RPAREN','expression',4,'p_expression_function','calc.py',163),
+  ('expression -> expression MULTIPLY expression','expression',3,'p_expression','calc.py',173),
+  ('expression -> expression DIVIDE expression','expression',3,'p_expression','calc.py',174),
+  ('expression -> expression PLUS expression','expression',3,'p_expression','calc.py',175),
+  ('expression -> expression MINUS expression','expression',3,'p_expression','calc.py',176),
+  ('expression -> INT','expression',1,'p_expression_int_float','calc.py',183),
+  ('expression -> FLOAT','expression',1,'p_expression_int_float','calc.py',184),
+  ('expression -> NAME','expression',1,'p_expression_var','calc.py',190),
+  ('empty -> <empty>','empty',0,'p_empty','calc.py',201),
 ]
